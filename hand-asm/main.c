0fcccca6a3b9d1c5b01ad608726938013e4e748a
ryotarokawakami <ryotaro.kawakami@misoca.jp>
2020-03-08 09:16:54 +0900

    add hand-asm

---
+++ hand-asm/main.c
@@ -0,0 +1,8 @@
+extern void test_func(void);
+extern void test_func2(void);
+
+int main() {
+    test_func2();
+    test_func();
+    return 0;
+}
---
+++ hand-asm/test_func2.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+void test_func2(void) {
+    printf("a = %lx, b = %lx\n", 0x12ffffffffffff34, 0x56ffffffffffff78);
+}
+
+typedef void (pf)(void);
+
+void test_func3(void) {
+    pf *p = test_func2;
+    (*p)();
+}
